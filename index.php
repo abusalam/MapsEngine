2b29d196eecfa99f9adab58877efda793b81e75d
Abu Salam <abusalamparvezalam@gmail.com>
2014-05-28 14:11:03 +0530

    Added Marker and InfoWindow

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $Data = new MySQLiDBHelper();
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta charset="utf-8">
     <style type="text/css">
       html { height: 100% }
       body { height: 100%; margin: 0; padding: 0 }
@@ -21,14 +22,44 @@ $Data = new MySQLiDBHelper();
     <script type="text/javascript"
             src="https://maps.googleapis.com/maps/api/js?key=&sensor=TRUE">
     </script>
-    <script type="text/javascript">
+    <script type="text/javascript" charset="UTF-8">
       function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(22.207, 87.903),
+          center: new google.maps.LatLng(22.4333, 87.3333),
           zoom: 10
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
                 mapOptions);
+
+        var myLatlng = new google.maps.LatLng(22.4333, 87.3333);
+
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Paschim Medinipur'
+        });
+
+        var contentString = '<div id="content">' +
+                '<div id="siteNotice">' +
+                '</div>' +
+                '<h1 id="firstHeading" class="firstHeading">Paschim Medinipur district</h1>' +
+                '<div id="bodyContent">' +
+                '<p>Paschim Medinipur district or West Midnapore district (Bengali: পশ্চিম মেদিনীপুর জেলা) ' +
+                '(also known as Midnapore West) is the districts of the state of West Bengal, ' +
+                'India. It was formed on January 1, 2002 after the Partition of Midnapore into ' +
+                'Paschim Medinipur and Purba Medinipur. The district has 4 sub-divisions: Kharagpur, ' +
+                'Medinipur Sadar, Ghatal and Jhargram. It is currently a part of the Red Corridor.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map, marker);
+        });
+
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
